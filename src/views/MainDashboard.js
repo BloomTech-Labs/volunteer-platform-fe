608de0aab0f46105193795cc492d3392cb2f41f0
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-09-16 11:53:53 -0600

    Changed Main Dashboard to conditionally render login logout and create org

--- src/views/MainDashboard.js
+++ src/views/MainDashboard.js
@@ -1,15 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { StyledButton } from "../styled";
-import { signOut } from "../actions";
-import { useStateValue } from "../hooks/useStateValue";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { StyledButton } from '../styled';
+import { signOut } from '../actions';
+import { useStateValue } from '../hooks/useStateValue';
 
 const MainDashboard = () => {
   const [ state, dispatch ] = useStateValue();
   return ( <div>
-    <Link to={ "/signup" }/>
-    <Link to={ "/login" }/>
-    <StyledButton onClick={ () => signOut( dispatch ) }>Log out</StyledButton>
+    { !state.auth.loggedIn ? ( <><Link to={ '/login' }/>
+        <Link to={ '/create-org' }/>
+      </> ) : <StyledButton onClick={ () => signOut( dispatch ) }>Log
+      out</StyledButton> }
     <h1>Shows a list of events.</h1>
   </div> );
 };
