8aaa25f3cfe2541f6b7b029f492ca3f4bc20ccde
Oscar Pacheco <Oscarpa88@yahoo.com>
2019-09-18 11:47:30 -0500

    Updated Login.spec.js

--- src/__tests__/Login.spec.js
+++ src/__tests__/Login.spec.js
@@ -4,31 +4,31 @@ import Login from '../views/Login';
 import { StateProvider } from '../contexts/StateProvider';
 
 describe( 'Login Page', () => {
-  
+
   it( 'properly renders', async() => {
     const login = render( <StateProvider> <Login/> </StateProvider> );
     expect( login.getByText( /login with/i ) ).toBeInTheDocument();
-    
+
   } );
-  
+
   it( 'to have a google button', () => {
     const login = render( <StateProvider> <Login/> </StateProvider> );
     const googleLoginButton = login.getByText( /Google/ );
     expect( googleLoginButton ).toBeVisible();
   } );
-  
+
   it( 'to have a facebook button', () => {
     const login = render( <StateProvider> <Login/> </StateProvider> );
     const facebookButton = login.getByText( /facebook/i );
     expect( facebookButton ).toBeVisible();
   } );
-  
-  it( 'to have a facebook button', () => {
+
+  it( 'to have a twitter button', () => {
     const login = render( <StateProvider> <Login/> </StateProvider> );
     const twitterButton = login.getByText( /twitter/i );
     expect( twitterButton ).toBeVisible();
   } );
-  
+
   it( 'should have a user name and password text input', () => {
     const login = render( <StateProvider> <Login/> </StateProvider> );
     const emailInput = login.getByTitle( /email/ );
