ec2166c24f328bc7386529dad309e3612e7eda53
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-09-17 20:20:39 -0600

    Created a StateProvider to encapsulate our components to be rendered to the screen.

--- src/__tests__/Login.spec.js
+++ src/__tests__/Login.spec.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Login from '../views/Login';
+import { StateProvider } from '../contexts/StateProvider';
 
 describe( 'Login Page', () => {
   it( 'properly renders', async() => {
-   console.log("Starting the test");
-    const login = render( <Login/> );
+    const login = render( <StateProvider> <Login/> </StateProvider> );
+    expect( login.getByText( /login with/i ) ).toBeInTheDocument();
+    
+  } );
+  
+  it( 'to have a google button', () => {
+    const login = render( <StateProvider> <Login/> </StateProvider> );
+    const googleLoginButton = login.getByText( /Google/ );
+    expect( googleLoginButton ).toBeVisible();
+  } );
+  
+  it( 'to have a facebook button', () => {
+    const login = render( <StateProvider> <Login/> </StateProvider> );
+    const facebookButton = login.getByText( /facebook/i );
+    expect( facebookButton ).toBeVisible();
   } );
 } );
