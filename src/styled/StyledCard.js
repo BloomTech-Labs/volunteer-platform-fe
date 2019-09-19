1834c9d624dd46f745f50cee8ec812c739973dbb
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-09-19 17:19:22 -0400

    Add box shadow and hover

--- src/styled/StyledCard.js
+++ src/styled/StyledCard.js
@@ -6,6 +6,11 @@ export const StyledCard = styled(Card)`
     width: 100%;
     max-width: 500px;
     border-radius: ${({theme}) => theme.borderRadiusDefault};
-    background: ${({theme}) => theme.secondary300}
+    box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+    transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+    cursor: pointer;
+    &:hover{
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+    }
   }
 `;
