e9881ae26d8caf0434127a6d5933393d961de84e
mochibot <amusaur83@gmail.com>
2019-09-20 11:42:51 -0700

    saving local changes before pull staging

--- src/App.js
+++ src/App.js
@@ -49,7 +49,6 @@ function App(){
   }, [] );
   
   useEffect( () => {
-    debugger;
     if( state.auth.googleAuthUser && state.auth.googleAuthUser.uid ){
       subscribeToUserOrganizations( state.auth.googleAuthUser.uid, dispatch );
     }
