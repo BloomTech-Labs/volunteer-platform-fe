490429326734e4e33d5529236abc425ca4674c4c
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-09-24 14:26:49 -0400

    Pull in info for edit Org

--- src/styled/AntdForm.js
+++ src/styled/AntdForm.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { Form, Icon, Tooltip } from 'antd';
 import { StyledButton } from '../styled';
-
+import moment from 'moment';
 class AntdForm extends React.Component {
+  componentDidUpdate(prevProps) {
+    if (prevProps.editInfo !== this.props.editInfo) {
+      for (let key in this.props.editInfo) {
+        this.props.form.setFieldsValue({ [key]: this.props.editInfo[key] });
+      }
+    }
+  }
+
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFieldsAndScroll((err, values) => {
