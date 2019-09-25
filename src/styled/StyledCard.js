e6b21d2f9e07ae25e5038c3f82a48fcb28a21679
Blake Goms <blakegoms@gmail.com>
2019-09-25 15:59:58 -0700

    Basic Styling Done

--- src/styled/StyledCard.js
+++ src/styled/StyledCard.js
@@ -10,8 +10,20 @@ export const StyledCard = styled(Card)`
     box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
     transition: all 0.3s cubic-bezier(0.25, 0.8, 0.25, 1);
     cursor: pointer;
+    margin-bottom: 15px;
+    letter-spacing: .2px;
     &:hover {
       box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+    }
+
+    .date {
+      display:flex;
+      justify-content: space-evenly;
+    }
+
+    .contact {
+      display: flex;
+      justify-content: space-evenly;
     }
   }
 `;
