18cfb21236ce90fdf5a2f8efb9a395c29070db99
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-09-26 16:44:47 -0400

    Find next Date for custom monthly events

--- src/utility/findNextRecurEvent.js
+++ src/utility/findNextRecurEvent.js
@@ -1,10 +1,9 @@
 import moment from 'moment';
 import { findNextCustom } from './findNextCustom';
+import { findNthWeek } from './findNthWeek';
 
 export const findNext = (date, info) => {
   let keyWord = info.repeatTimePeriod.split(' ')[0];
-  let dayAbbrevs = { Sun: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6 };
-  let unitConversion = { First: 1, Second: 2, Third: 3, Fourth: 4, Fifth: 5 };
   switch (keyWord) {
     case 'Custom':
       return findNextCustom(date, info);
@@ -18,38 +17,7 @@ export const findNext = (date, info) => {
         .day(weekdayOfEvent + 7)
         .format('LL');
     case 'Monthly':
-      let whichWeek = unitConversion[info.repeatTimePeriod.split(' ')[3]];
-      let whichDay = dayAbbrevs[info.repeatTimePeriod.split(' ')[4]];
-      let first_week = moment()
-        .startOf('month')
-        .day(whichDay);
-      let correct_week = first_week.add(7 * (whichWeek - 1), 'days');
-      if (correct_week.month() !== moment().month()) {
-        correct_week = 0;
-      }
-      let firstOfNext;
-      if (moment().diff(correct_week) > 0) {
-        firstOfNext = moment()
-          .add(1, 'month')
-          .startOf('month')
-          .day();
-        if (firstOfNext > whichDay) {
-          first_week = moment()
-            .add(1, 'month')
-            .startOf('month')
-            .day(whichDay + 7);
-          return first_week.add(7 * (whichWeek - 1), 'days').format('LL');
-        } else {
-          return moment()
-            .add(1, 'month')
-            .startOf('month')
-            .day(whichDay)
-            .add(7 * (whichWeek - 1), 'days')
-            .format('LL');
-        }
-      } else {
-        return correct_week.format('LL');
-      }
+      return findNthWeek(date, info, 1);
     case 'Annually':
       return moment
         .unix(date)
---
+++ src/utility/findNthWeek.js
@@ -0,0 +1,33 @@
+import moment from 'moment';
+
+export const findNthWeek = (date, info, period) => {
+  let dayAbbrevs = { Sun: 0, Mon: 1, Tue: 2, Wed: 3, Thu: 4, Fri: 5, Sat: 6 };
+  let unitConversion = { First: 1, Second: 2, Third: 3, Fourth: 4 };
+  let whichWeek = unitConversion[info.repeatTimePeriod.split(' ')[3]];
+  let whichDay = dayAbbrevs[info.repeatTimePeriod.split(' ')[4]];
+  let first_week = moment()
+    .startOf('month')
+    .day(whichDay);
+  let correct_week = first_week.add(7 * (whichWeek - 1), 'days');
+  let firstOfNext;
+  while (moment().diff(correct_week) > 0) {
+    firstOfNext = moment()
+      .add(period, 'month')
+      .startOf('month')
+      .day();
+    if (firstOfNext > whichDay) {
+      first_week = moment()
+        .add(period, 'month')
+        .startOf('month')
+        .day(whichDay + 7);
+      correct_week = first_week.add(7 * (whichWeek - 1), 'days');
+    } else {
+      correct_week = moment()
+        .add(period, 'month')
+        .startOf('month')
+        .day(whichDay)
+        .add(7 * (whichWeek - 1), 'days');
+    }
+  }
+  return correct_week.format('LL');
+};
