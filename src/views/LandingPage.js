f84afbeb1d356c100721c9f84a906872e1d692ac
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-09-30 16:47:51 -0400

    Redesign hero content on landing page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,39 +1,97 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import heroImage from '../assets/hero_image4.png';
-import {device} from '../styled/deviceBreakpoints';
+import { device } from '../styled/deviceBreakpoints';
 import {
-  HowItWorks, TopVolunteers, TopNonProfits,
+  HowItWorks,
+  TopVolunteers,
+  TopNonProfits,
 } from '../components/LandingPage';
-import {useStateValue} from '../hooks/useStateValue'
+import { useStateValue } from '../hooks/useStateValue';
+import { Input, message, Icon } from 'antd';
+import { stateConversion } from '../utility/stateConversion';
+
+const { Search } = Input;
 
 export const LandingPage = () => {
-    const [{auth}] = useStateValue()
+  const [{ auth }] = useStateValue();
+  const [location, setLocation] = useState('');
 
-  return (<>
-    <StyledHeroDiv image={heroImage} loggedIn = {auth.loggedIn}>
-      <HeroContent>
-        <p>Meet new friends, give back to the community.</p>
-        <p>Win-win.</p>
-        <button>
-          <Link to="/dashboard">Get Started</Link>
-        </button>
-      </HeroContent>
-    </StyledHeroDiv>
-    <ContentDiv>
-      <HowItWorks/>
-      <TopVolunteers/>
-      <TopNonProfits/>
-    </ContentDiv>
-  </>);
+  const handleSearch = e => {
+    
+  };
+
+  const handleChange = e => {
+    setLocation(e.target.value);
+  };
+  useEffect(() => {
+    axios
+      .get(`https://geoip-db.com/json/${process.env.REACT_APP_ipinfoKey}`)
+      .then(res => {
+        let stateAbbrev = Object.keys(stateConversion).find(
+          key => stateConversion[key] === res.data.state
+        );
+        let userCity = res.data.city;
+        if (stateAbbrev) {
+          setLocation(`${userCity}, ${stateAbbrev}`);
+        } else {
+          message.warning(
+            'Unable to get your location. Please enter your state below.'
+          );
+        }
+      })
+      .catch(err => {
+        console.log('Error detecting location');
+        message.warning(
+          'Unable to get your location. Please enter your state below.'
+        );
+      });
+  }, []);
+
+  return (
+    <>
+      <StyledHeroDiv image={heroImage} loggedIn={auth.loggedIn}>
+        <HeroContent loggedIn={auth.loggedIn}>
+          <p>
+            Compete with friends, meet new ones, give back to the community.
+          </p>
+          <p>Win-win.</p>
+          <Search
+            placeholder="Enter your city, state"
+            onSearch={handleSearch}
+            enterButton={
+              <Link
+                to={{
+                  pathname: '/dashboard',
+                  state: { userLocation: location },
+                }}
+              >
+                <Icon type="search" />
+              </Link>
+            }
+            size="large"
+            style={{ width: '25%' }}
+            onChange={handleChange}
+            value={location}
+          />
+        </HeroContent>
+      </StyledHeroDiv>
+      <ContentDiv>
+        <HowItWorks />
+        <TopVolunteers />
+        <TopNonProfits />
+      </ContentDiv>
+    </>
+  );
 };
 
 export default LandingPage;
 
 const StyledHeroDiv = styled.div`
   height: 50vh;
-  width: ${({loggedIn}) => loggedIn && '100vw'};
+  width: ${({ loggedIn }) => loggedIn && '100vw'};
   display: flex;
   position: relative;
   justify-content: center;
@@ -59,11 +117,10 @@ const StyledHeroDiv = styled.div`
 
 const HeroContent = styled.div`
   width: 100%;
-  text-align: left;
-  margin: 0 0 0 6rem;
+  text-align: center;
   color: white;
   z-index: 10;
-  padding: 0px 10px;
+  padding-right: ${({ loggedIn }) => loggedIn && '15rem'};
 
   @media ${device.laptop} {
   }
@@ -79,18 +136,19 @@ const HeroContent = styled.div`
   }
 
   button {
-    background: ${({theme}) => theme.accent};
-    border-radius: 5px;
+    background: ${({ theme }) => theme.accent};
+    border-radius: inherit;
     padding: 10px 20px;
     border: 0;
     cursor: pointer;
     font-family: 'Arvo', serif;
     width: 165px;
-    font-size: 20px;
-    margin-top: 30px;
 
+    .anticon {
+      font-size: 25px;
+    }
     &:hover {
-      background: ${({theme}) => theme.accent7};
+      background: ${({ theme }) => theme.accent7};
     }
 
     a {
