97f8a12c9918a2abcc930b7c7565eb386e7c81a5
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-10-02 18:51:50 -0400

    Add limit on phone number length when typing

--- src/views/CreateOrg.js
+++ src/views/CreateOrg.js
@@ -49,7 +49,7 @@ export const CreateOrg = props => {
 
   const steps = [0, 1, 2, 3, 4];
 
-  const RenderedPart = possibleParts[2];
+  const RenderedPart = possibleParts[partCount];
 
   const clickNext = values => {
     if (partCount === 2) {
