d2de98dd020b0af5bf2062d8367a68466ce6da27
mochibot <amusaur83@gmail.com>
2019-10-02 13:12:32 -0700

    added upload photo functionality to user profile

--- src/components/UserProfile/UserBio.js
+++ src/components/UserProfile/UserBio.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { AntTextArea } from '../../styled/index';
 
 export const UserBio = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    if (!props.bio) {
+      setIsEditing(true);
+    }
+  }, [props.bio])
+
   return (
-    <div>
+    <StyledDiv>
       <p>Bio</p>
-      <p>{props.bio ? 'Add a bio' : props.bio}</p>
-    </div>
+      {isEditing ? (
+        <AntTextArea 
+          name='bio'
+          autosize={{ minRows: 7}}
+          placeholder='Add a blurb about yourself here'
+        />
+      )
+      : <p>props.bio</p>}
+    </StyledDiv>
   )
 }
 
 export default UserBio;
+
+const StyledDiv = styled.div`
+  background: red;
+  width: 40%;
+  padding: 2rem;
+`
