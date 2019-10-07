9e332d060f810c3f7baf99a17059b56bc0c92f0a
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-10-07 16:20:54 -0400

    Redesign findNext to take in a dynamic timestamp

--- src/utility/findNextRecurEvent.js
+++ src/utility/findNextRecurEvent.js
@@ -2,87 +2,60 @@ import moment from 'moment';
 import { findNextCustom } from './findNextCustom';
 import { findNthWeek } from './findNthWeek';
 
-export const findNext = (date, info) => {
-  let keyWord = info.repeatTimePeriod.split(' ')[0];
+export const eventPassed = date => {
+  return moment().unix() - date > 0;
+};
+
+export const findNext = (date, keyWord, info = {}) => {
   switch (keyWord) {
-    case 'Custom':
+    case 'Custom': //need to do
       return findNextCustom(date, info);
     case 'Daily':
-      return moment()
-        .add(1, 'days')
-        .unix();
+      return date.add(1, 'days');
     case 'Weekly':
-      let weekdayOfEvent = moment.unix(date).day();
-      return moment()
-        .day(weekdayOfEvent + 7)
-        .unix();
-    case 'Monthly':
+      let weekdayOfEvent = date.day();
+      return date.day(weekdayOfEvent + 7);
+    case 'Monthly': //need to do
       return findNthWeek(moment().unix(), info, 1);
     case 'Annually':
-        //need to fix this to see if the date stored has occurred yet, if not then add a year
-      return moment
-        .unix(date)
-        .add(1, 'year')
-        .unix();
+      return date.add(1, 'year');
     case 'Weekdays':
-      let today = moment().day();
-      if (today === 5 || today === 6) {
-        return moment()
-          .day(8)
-          .unix();
+      let dayOfWeek = date.day();
+      if (dayOfWeek === 5 || dayOfWeek === 6) {
+        //Fri or Sat
+        return date.day(8);
       } else {
-        return moment()
-          .add(1, 'days')
-          .unix();
+        return date.add(1, 'days');
       }
   }
 };
 
 export const findNextEvents = event => {
+  debugger;
   let keyWord = event.recurringInfo.repeatTimePeriod.split(' ')[0];
   event.registeredVolunteers = event.registeredVolunteers || {};
+  let passed = eventPassed(event.startTimeStamp);
+  let eventDay = moment.unix(event.startTimeStamp);
+  if (passed) {
+    eventDay = findNext(
+      moment.unix(event.startTimeStamp),
+      keyWord,
+      event.recurringInfo
+    );
+  }
   let arrayOfDates = event.registeredVolunteers
     ? [...Object.keys(event.registeredVolunteers)].sort()
     : [];
   let end = findEndDate(event.recurringInfo, arrayOfDates);
   if (!end) return event;
   arrayOfDates = arrayOfDates.filter(timeStamp => moment().unix() < timeStamp);
-  let eventDay = findNext(event.date, event.recurringInfo);
 
   while (
-    end.maxDate.diff(moment.unix(eventDay).startOf('day')) > 0 &&
+    end.maxDate.diff(eventDay.startOf('day')) > 0 &&
     arrayOfDates.length < end.maxEvents
   ) {
-    arrayOfDates.push(eventDay);
-    switch (keyWord) {
-      case 'Daily':
-        eventDay = moment
-          .unix(eventDay)
-          .add(1, 'day')
-          .unix();
-        break;
-      case 'Weekly':
-        eventDay = moment
-          .unix(eventDay)
-          .add(1, 'week')
-          .unix();
-        break;
-      case 'Annually':
-        eventDay = moment
-          .unix(eventDay)
-          .add(1, 'year')
-          .unix();
-        break;
-      case 'Monthly':
-        eventDay = findNthWeek(
-          moment
-            .unix(eventDay)
-            .add(1, 'month')
-            .unix(),
-          event.recurringInfo,
-          1
-        );
-    }
+    arrayOfDates.push(eventDay.unix());
+    eventDay = findNext(eventDay, keyWord, event.recurringInfo);
   }
   for (let key of arrayOfDates) {
     if (!event.registeredVolunteers[key]) {
