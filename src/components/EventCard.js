ee49e7bc5466daa70b380574ba9147673b64f276
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-10-07 15:25:40 -0400

    Reset input area after comment submits

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -1,25 +1,22 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import manHiking from '../assets/man-hiking.jpg';
 import styled from 'styled-components';
-import {StyledCard} from '../styled';
-import {Tag, AutoComplete, Icon} from 'antd';
+import { StyledCard } from '../styled';
+import { Tag, AutoComplete, Icon } from 'antd';
 import moment from 'moment';
-import {findNext} from '../utility/findNextRecurEvent';
+import { findNext } from '../utility/findNextRecurEvent';
 import PropTypes from 'prop-types';
 import Editor from './EventComments/Editor';
 import CommentList from './EventComments/CommentsList';
-import {useStateValue} from '../hooks/useStateValue';
-import {addComment, addCommentToComment} from '../actions';
+import { useStateValue } from '../hooks/useStateValue';
+import { addComment, addCommentToComment } from '../actions';
 import uuid4 from 'uuid4';
 
-export const EventCard = ({event}) => {
-  
-  const [{auth, comments}, dispatch] = useStateValue();
-  
-  useEffect(() => {
-  
-  }, [comments]);
-  
+export const EventCard = ({ event }) => {
+  const [{ auth, comments }, dispatch] = useStateValue();
+
+  useEffect(() => {}, [comments]);
+
   const [localState, setLocalState] = useState({
     interest: [],
     typesOfCauses: [],
@@ -34,85 +31,86 @@ export const EventCard = ({event}) => {
     nextDate: '',
     recurringInfo: {},
   });
-  
+
   useEffect(() => {
-    if ('recurringInfo' in event){
+    if ('recurringInfo' in event) {
       let nextDate = findNext(event.startTimeStamp, event.recurringInfo);
       setLocalState({
         ...localState,
         nextDate: moment(
-          moment.unix(nextDate).format('LL') + ' ' + event.startTime,
-        ).unix(), ...event,
+          moment.unix(nextDate).format('LL') + ' ' + event.startTime
+        ).unix(),
+        ...event,
       });
-    }else{
-      setLocalState({...localState, nextDate: event.startTimeStamp, ...event});
+    } else {
+      setLocalState({
+        ...localState,
+        nextDate: event.startTimeStamp,
+        ...event,
+      });
     }
   }, [event]);
-  
+
   const causes = localState.typesOfCauses.map(item => {
     return <Tag>{(item = [item])}</Tag>;
   });
-  
+
   const interest = localState.interest.map(item => {
     return <Tag>{(item = [item])}</Tag>;
   });
-  
+
   const requirements = localState.volunteerRequirements.map(item => {
     return <Tag>{(item = [item])}</Tag>;
   });
-  
-  console.log('LocalState', localState);
-  
-  const submitComment = (text) => {
-    
+
+  const submitComment = text => {
     const comment = {
       commentId: uuid4(),
       comment: text.comment,
       createdAt: moment().unix(),
       usersUid: auth.googleAuthUser.uid,
       name: auth.registeredUser.firstName + ' ' + auth.registeredUser.lastName,
+      avatarPath: `/images/${auth.googleAuthUser.uid}`,
     };
-    
-    if (auth.registeredUser.imagePath){
-      comment.avatarPath = auth.registeredUser.imagePath;
-    }
+
     addComment(comment, event, dispatch);
   };
-  
+
   const handleAddCommentToComment = (text, comment) => {
-    
     const newComment = {
       commentId: uuid4(),
       comment: text.comment,
       createdAt: moment().unix(),
       usersUid: auth.googleAuthUser.uid,
       name: auth.registeredUser.firstName + ' ' + auth.registeredUser.lastName,
+      avatarPath: `/images/${auth.googleAuthUser.uid}`,
     };
-    
-    if (auth.registeredUser.imagePath){
-      newComment.avatarPath = auth.registeredUser.imagePath;
-    }
-    
+
     addCommentToComment(newComment, event, comment, dispatch);
   };
-  
+
   const backButton = () => {
     //props.history.goBack();
     return null;
   };
-  
+
   return (
     <div>
       <div>
-        <Icon type="left-circle" theme="filled" onClick={backButton} style={{
-          marginLeft: '15%',
-          marginRight: '10px',
-          marginTop: '10px',
-          fontSize: '24px',
-        }}/>
+        <Icon
+          type="left-circle"
+          theme="filled"
+          onClick={backButton}
+          style={{
+            marginLeft: '15%',
+            marginRight: '10px',
+            marginTop: '10px',
+            fontSize: '24px',
+          }}
+        />
         Previous Page
       </div>
-      <div style={{margin: '0 auto', width: '70%'}}>
+      <div style={{ margin: '0 auto', width: '70%' }}>
         <div>
           <h4> {localState.nameOfEvent} </h4>
           <h6> {moment.unix(localState.nextDate).format('LLLL')} </h6>
@@ -121,7 +119,7 @@ export const EventCard = ({event}) => {
         <StyledEventPage>
           <div className="card">
             <div className="photo">
-              <img src={manHiking} alt="dude" width={250} height={250}/>
+              <img src={manHiking} alt="dude" width={250} height={250} />
             </div>
             <div className="tags">
               <h5>Interests: </h5>
@@ -133,19 +131,19 @@ export const EventCard = ({event}) => {
             </div>
           </div>
         </StyledEventPage>
-        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <StyledEventDetails>
             <div className="details">
               <h5>Details</h5>
             </div>
-            <div className="description">
-              {localState.eventDetails}
-            </div>
+            <div className="description">{localState.eventDetails}</div>
           </StyledEventDetails>
           <StyledEventTime>
             <div className="time">
-              <h6> Every Tuesday, Thursday,
-                Friday{localState.recurringInfo.days}</h6>
+              <h6>
+                {' '}
+                Every Tuesday, Thursday, Friday{localState.recurringInfo.days}
+              </h6>
             </div>
             <div className="info">
               <h5>
@@ -153,102 +151,103 @@ export const EventCard = ({event}) => {
               </h5>
             </div>
           </StyledEventTime>
-          <div style={{display: 'column'}}>
-            <Icon type="twitter-circle" theme="filled"/>
-            <Icon type="facebook" theme="filled"/>
-            <Icon type="google-circle" theme="filled"/>
+          <div style={{ display: 'column' }}>
+            <Icon type="twitter-circle" theme="filled" />
+            <Icon type="facebook" theme="filled" />
+            <Icon type="google-circle" theme="filled" />
           </div>
         </div>
       </div>
-      <CommentList comments={event.comments}
-                   addCommentToComment={handleAddCommentToComment}
-                   isLoading={comments.isLoadingReplyToComment}
-                   event={event}
+      <CommentList
+        comments={event.comments}
+        addCommentToComment={handleAddCommentToComment}
+        isLoading={comments.isLoadingReplyToComment}
+        event={event}
       />
-      <Editor onSubmit={submitComment} submitting={comments.isLoading}/>
+      <Editor onSubmit={submitComment} submitting={comments.isLoading} />
     </div>
   );
 };
 
 const StyledEventPage = styled(StyledCard)`
-.ant-card-body {
+  .ant-card-body {
     padding: 0;
-}
-&& {
+  }
+  && {
     border-radius: 2px;
     width: 100%;
     max-width: 100%;
     padding: 0;
-}   
-.card {
+  }
+  .card {
     display: flex;
     width: 100%;
     margin-bottom: 20px;
-    
+
     .photo {
-        margin-right: 3%;
+      margin-right: 3%;
     }
 
     .tags {
-        width: 75%;
-        flex-direction: column;
-        flex-wrap: wrap;
-        justify-content: center;
-        margin: 2% 0;
-        .subtag {
-            flex-direction: row;
-            justify-content: flex-start;
-        }
+      width: 75%;
+      flex-direction: column;
+      flex-wrap: wrap;
+      justify-content: center;
+      margin: 2% 0;
+      .subtag {
+        flex-direction: row;
+        justify-content: flex-start;
+      }
     }
-}
+  }
 `;
 
 const StyledEventDetails = styled(StyledCard)`
-.ant-card-body {
+  .ant-card-body {
     padding: 0;
-}
-&&& {
+  }
+  &&& {
     background-color: white;
     border-radius: 2px;
     margin-top: 1.5%;
     width: 100%;
 
     .container {
-        display: flex;
+      display: flex;
     }
     .details {
-        border-bottom: 1px solid black;
-        text-align:center;
+      border-bottom: 1px solid black;
+      text-align: center;
     }
 
     .description {
-        background-color: white;
-        padding: 1% 0;
-        text-align:left;
+      background-color: white;
+      padding: 1% 0;
+      text-align: left;
     }
-}
+  }
 `;
 
 const StyledEventTime = styled(StyledCard)`
-.ant-card-body {
+  .ant-card-body {
     padding: 0;
-}
-&&& {
+  }
+  &&& {
     background-color: white;
     border-radius: 2px;
     margin-top: 1.5%;
     width: 35%;
     height: 100px;
-    text-align:center;
+    text-align: center;
 
     .time {
-        border-bottom: 1px solid black;
+      border-bottom: 1px solid black;
     }
 
     .info {
-        background-color: white;
+      background-color: white;
     }
-}
+  }
 `;
 
 EventCard.propTypes = {
--- src/components/EventComments/Editor.js
+++ src/components/EventComments/Editor.js
@@ -1,24 +1,28 @@
-import React, {useState} from 'react';
-import {WrappedAntForm, AntTextArea} from '../../styled';
+import React, { useState } from 'react';
+import { WrappedAntForm, AntTextArea } from '../../styled';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Editor = ({onSubmit, submitting}) => {
+const Editor = ({ onSubmit, submitting }) => {
   return (
     <SyledEditor>
-      <WrappedAntForm onSubmit={onSubmit} submitButtonText={'Send'}
-                      buttonType={'submit'}
-                      submitButton={true} buttonLoading={submitting}
+      <WrappedAntForm
+        onSubmit={onSubmit}
+        submitButtonText={'Send'}
+        buttonType={'submit'}
+        submitButton={true}
+        buttonLoading={submitting}
+        autofill={submitting && { comment: '' }}
       >
-        <AntTextArea name={'Comment'}/>
+        <AntTextArea name={'Comment'} />
       </WrappedAntForm>
-    
-    </SyledEditor>);
+    </SyledEditor>
+  );
 };
 
 const SyledEditor = styled.div`
-margin-left: 15rem;
-padding-left: 5rem;
+  margin-left: 15rem;
+  padding-left: 5rem;
 `;
 
 Editor.propTypes = {
