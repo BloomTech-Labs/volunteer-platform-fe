ff7585f68b0b9a760c71849f4bee5f9c93afa36d
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-10-08 12:32:13 -0400

    Update findNextEvents for recur events

--- src/actions/events.js
+++ src/actions/events.js
@@ -205,13 +205,12 @@ export const getAllRecurringEventsByState = (state, dispatch) => {
         res.forEach(event => {
           const data = event.data();
           data.eventId = event.id;
-          let daysOfEvents = [];
-          if (data.recurringInfo.repeatTimePeriod.includes('Sat/Sun')) {
-            data.registeredVolunteers = findNextEvents(data);
-            event.ref.update({
-              registeredVolunteers: data.registeredVolunteers,
-            });
-          }
+
+          data.registeredVolunteers = findNextEvents(data);
+          event.ref.update({
+            registeredVolunteers: data.registeredVolunteers,
+          });
+
           events.push(data);
         });
 
