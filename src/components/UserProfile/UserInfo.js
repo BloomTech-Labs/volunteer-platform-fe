0cebc3784185424257d38c0f79ca537fc6df00af
mochibot <amusaur83@gmail.com>
2019-10-16 12:09:40 -0700

    minor tweak to fix the border radius of userInfo

--- src/components/UserProfile/UserInfo.js
+++ src/components/UserProfile/UserInfo.js
@@ -179,6 +179,7 @@ const StyledDiv = styled.div`
 const CustomStyledCard = styled(StyledCard)`
   && {
     background: ${({theme}) => theme.gray2};
+    border-radius: 3px;
   }
 
   .ant-card-body {
